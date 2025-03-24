274763b42ee9d33917115496dd97a436d57c7a3d
Lohansa Munasinghe <119659122+lohansa@users.noreply.github.com>
2025-03-24 20:07:23 +0530

    updated footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -10,13 +10,13 @@ const Footer = () => {
         <div className="max-w-5xl mx-auto flex flex-col md:flex-row justify-between items-center">
           <div className="mb-4 md:mb-0">
             <p className="text-center md:text-left text-muted-foreground">
-              © {currentYear} John Doe. All rights reserved.
+              © {currentYear} Lohansa Munasinghe. All rights reserved.
             </p>
           </div>
           
           <div className="flex items-center">
             <p className="text-center md:text-right text-muted-foreground flex items-center gap-1">
-              Made with <Heart size={14} className="text-red-500" /> in San Francisco
+              Made with <Heart size={14} className="text-red-500" /> in Colombo, SriLanka
             </p>
           </div>
         </div>
